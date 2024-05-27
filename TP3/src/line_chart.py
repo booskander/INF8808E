80018e1836d6d748d1a53c28ace596879b90439c
nada alami chentoufi <89505314+MRX418@users.noreply.github.com>
2024-05-27 13:05:14 -0400

    changements line chart faits

--- TP3/src/line_chart.py
+++ TP3/src/line_chart.py
@@ -7,18 +7,40 @@ import hover_template
 from template import THEME
 
 
-def get_empty_figure():
+def get_empty_figure(height=400):
     '''
-        Returns the figure to display when there is no data to show.
+    Returns the figure to display when there is no data to show.
 
-        The text to display is : 'No data to display. Select a cell
-        in the heatmap for more information.
+    The text to display is : 'No data to display. Select a cell
+    in the heatmap for more information.
 
     '''
 
-    # TODO : Construct the empty figure to display. Make sure to 
-    # set dragmode=False in the layout.
-    return None
+    fig = px.scatter()
+
+    fig.add_annotation(
+        x=0.5,
+        y=0.5,  
+        text="No data to display. Select a cell in the heatmap for more information.",
+        showarrow=False,
+        font=dict(
+            family="Arial",
+            size=14,
+            color="black"
+        ),
+        xref="paper",  
+        yref="paper",  
+    )
+
+    fig.update_layout(
+        dragmode=False,
+        xaxis=dict(visible=False),
+        yaxis=dict(visible=False),
+        height=height,
+        showlegend=False
+    )
+
+    return fig
 
 
 def add_rectangle_shape(fig):
@@ -32,7 +54,25 @@ def add_rectangle_shape(fig):
         0.25% to 0.75% the height of the figure.
     '''
     # TODO : Draw the rectangle
-    return None
+    layout = fig.layout
+
+    fig_height = layout.height
+    rect_height = [0.25 * fig_height, 0.75 * fig_height]
+
+    pale_color = THEME['pale_color'] 
+
+    fig.add_shape(
+        type="rect",
+        x0=0,
+        y0=rect_height[0],
+        x1=1,
+        y1=rect_height[1],
+        line=dict(width=0),
+        fillcolor=pale_color,
+        layer="below"
+    )
+
+    return fig
 
 
 def get_figure(line_data, arrond, year):
@@ -57,4 +97,28 @@ def get_figure(line_data, arrond, year):
             The figure to be displayed
     '''
     # TODO : Construct the required figure. Don't forget to include the hover template
-    return None
+    fig = px.line(
+        line_data,
+        x='Date', y='Trees',
+        labels={'Date': 'Date', 'Trees': 'Trees'},
+        title=f'Trees planted in {arrond} in {year}',
+        template='custom_theme'
+    )
+
+    if len(line_data) == 1:
+        fig.update_traces(mode='markers')
+
+    fig.update_layout(
+        xaxis=dict(
+            tickformat='%d %b',
+            title=''
+        ),
+        yaxis=dict(
+            title='Trees'
+        )
+    )
+
+    fig.update_traces(hovertemplate=hover_template.get_linechart_hover_template())
+
+    return fig
+
